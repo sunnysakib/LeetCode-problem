0e74e6baa60e02f5f2579cc7a0cb9ce444f7c2e4
sunnysakib <sakibur.rahaman033@gmail.com>
2023-08-20 14:31:49 +0600

    contains-duplicate better solution impelement

--- javaScript/217. Contains Duplicate.js
+++ javaScript/217. Contains Duplicate.js
@@ -4,6 +4,7 @@
 https://leetcode.com/problems/contains-duplicate/
  */
 
+// solution 1 ---> Time complexity: O(n^2)
 var containsDuplicate = function(nums) {
     let flag=nums.length-1;
     for(let i=0; i<nums.length; i++){
@@ -16,5 +17,18 @@ var containsDuplicate = function(nums) {
             }
         }
     }
+};
+
+
+// solution 2 ---> Time complexity: O(n)
+var containsDuplicate = function(nums) {
+    let collection = new Map();
+     for(let i=0; i<nums.length; i++){
+       collection.set(nums[i], nums[i]);
+    }
+    if(collection.size === nums.length){
+        return false;
+    }
+    return true;
 };
 
