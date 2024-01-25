d26586859c0b4d10055dc0d9b05ad388c0184e27
sunnySakib <sakibur.rahaman033@gmail.com>
2024-01-25 14:02:02 +0600

    reverse function added

--- Data-structure/singly-linkedList_implement.js
+++ Data-structure/singly-linkedList_implement.js
@@ -88,6 +88,24 @@ class Node {
       }
       return array;
     }
+
+    reverse(){
+    if(!this.head.next){
+      return this.head;
+    }
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next; 
+    while(second){
+      let temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this.printList();
+  }
   }
   
   const myLinkList = new LinkList(10);
